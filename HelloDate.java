491efe086d8726e97e7a8fa7d21e7f96d92ad328
unknown <puruaws@gmail.com>
2022-05-27 21:53:52 +0530

    method print added

--- HelloDate.java
+++ HelloDate.java
@@ -4,5 +4,12 @@ public class HelloDate
 	public static void main(String a[])
 	{
 		System.out.println("Welcome to Jenkins "+new Date());
+		print(33);
+	}
+
+	public static void print(int x)
+	{
+	for(int i=1;i<=10;i++)
+	System.out.print(x+"x"+i+"=>"+x*i);
 	}
 }
